c61b6432565950ef4b125d470c5bfbf7a008c557
joe <164544303+johannes030305@users.noreply.github.com>
2024-05-21 19:27:13 +0700

    Add files via upload

---
+++ ya/index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <script src="https://kit.fontawesome.com/64d58efce2.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="style.css" />
+    <title>Sign in & Sign up Form</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300..700&display=swap" rel="stylesheet">
+  </head>
+  <body>
+
+  <?php
+        if (isset($_GET['alert'])) {
+          if ($_GET['alert'] == "gagal") {
+            echo "<div class='alert alert-danger'>LOGIN GAGAL! USERNAME DAN PASSWORD SALAH!</div>";
+          } else if ($_GET['alert'] == "logout") {
+            echo "<div class='alert alert-success'>ANDA TELAH BERHASIL LOGOUT</div>";
+          } else if ($_GET['alert'] == "belum_login") {
+            echo "<div class='alert alert-warning'>ANDA HARUS LOGIN UNTUK MENGAKSES DASHBOARD</div>";
+          }else if ($_GET['alert'] == "bukan_admin") {
+            echo "<div class='alert alert-warning'>ANDA BUKAN ADMIN</div>";
+          }
+        }
+        ?>
+
+    <div class="container">
+      <div class="forms-container">
+        <div class="signin-signup">
+          <form action="periksa_login.php" method="POST" class="sign-in-form">
+            <h2 class="title">Sign in</h2>
+            <div class="input-field">
+              <i class="fas fa-user"></i>
+              <input type="text" name="username" placeholder="Username" />
+            </div>
+            <div class="input-field">
+              <i class="fas fa-lock"></i>
+              <input type="password" name="password" placeholder="Password" />
+            </div>
+            <input type="submit" value="Login" class="btn solid" />
+          </form>
+        </div>
+      </div>
+
+      <div class="panels-container">
+        <div class="panel left-panel">
+          <div class="content">
+            <h3>New here ?</h3>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis, ex ratione. Aliquid!</p>
+          
+          <img src="logo.png" class="image" alt="" /></div>
+        </div>
+      </div>
+    </div>
+
+    <script src="app.js"></script>
+  </body>
+</html>
